0f482081b90596882ec731bb209bbb5c57f2e6d5
Karl Hiner <karl.hiner@gmail.com>
2013-06-23 23:02:04 -0700

    made 'widgets' dir, moving graph into its own file, adding yaml.js dependency for future yaml conversion stuff, adding gauge widget.

---
+++ frontend/widgets/gauge.js
@@ -0,0 +1,215 @@
+function Gauge(configuration) {
+    var self = this;
+
+    this.value = 0;
+
+    this.config = configuration;
+
+    this.config.labelTextConfig = {
+        size: Math.round(this.config.size / 9),
+        x: this.config.size / 2,
+        y: 3 * this.config.size / 10,
+        anchor: "middle"
+    };
+
+    this.config.valueTextConfig = {
+        size: Math.round(this.config.size / 10),
+        x: this.config.size / 2,
+        y: 4 * this.config.size / 5,
+        anchor: "middle"
+    };
+
+    this.config.radius = this.config.size * 0.97 / 2;
+
+    this.config.min = configuration.min || 0;
+    this.config.max = configuration.max || 100;
+    this.config.range = this.config.max - this.config.min;
+
+    this.config.majorTicks = configuration.majorTicks || 5;
+    this.config.minorTicks = configuration.minorTicks || 2;
+
+    this.config.greenColor  = "#109618";
+    this.config.yellowColor = "#FF9900";
+    this.config.redColor    = "#DC3912";
+
+    this.render = function() {
+
+        this.svg = d3.select("body").append("svg")
+            .attr("class", "gauge")
+            .attr("width", this.config.size)
+            .attr("height", this.config.size)
+            .style("background-color", "white");
+
+        this.svg.append("circle")
+        .attr("cx", this.config.size / 2)
+        .attr("cy", this.config.size / 2)
+        .attr("r", this.config.radius)
+        .style("fill", "#ccc")
+        .style("stroke", "#000")
+        .style("stroke-width", "0.5px");
+
+        this.svg.append("circle")
+        .attr("cx", this.config.size / 2)
+        .attr("cy", this.config.size / 2)
+        .attr("r", 0.9 * this.config.radius)
+        .style("fill", "#fff")
+        .style("stroke", "#e0e0e0")
+        .style("stroke-width", "2px");
+
+
+        //this.drawBand(this.config.greenZones[index].from, this.config.greenZones[index].to, self.config.greenColor);
+
+        var yellowFrom = (this.config.yellowPercent / 100.0) * this.config.max;
+        var yellowTo = (this.config.redPercent / 100.0) * this.config.max;
+        var redFrom = (this.config.redPercent / 100.0) * this.config.max;
+        var redTo = this.config.max;
+
+        this.drawBand(yellowFrom, yellowTo, self.config.yellowColor);
+
+        this.drawBand(redFrom, redTo, self.config.redColor);
+
+        this.appendText(this.svg, this.config.label, this.config.labelTextConfig);
+
+        var majorDelta = this.config.range / (this.config.majorTicks - 1);
+        for (var major = this.config.min; major <= this.config.max; major += majorDelta) {
+            var minorDelta = majorDelta / this.config.minorTicks;
+            for (var minor = major + minorDelta; minor < Math.min(major + majorDelta, this.config.max); minor += minorDelta) {
+                var point1 = this.valueToPoint(minor, 0.75);
+                var point2 = this.valueToPoint(minor, 0.85);
+
+                this.svg.append("line")
+                .attr("x1", point1.x)
+                .attr("y1", point1.y)
+                .attr("x2", point2.x)
+                .attr("y2", point2.y)
+                .style("stroke", "#666")
+                .style("stroke-width", "1px");
+            }
+
+            var point1 = this.valueToPoint(major, 0.7);
+            var point2 = this.valueToPoint(major, 0.85);
+
+            this.svg.append("line")
+            .attr("x1", point1.x)
+            .attr("y1", point1.y)
+            .attr("x2", point2.x)
+            .attr("y2", point2.y)
+            .style("stroke", "#333")
+            .style("stroke-width", "2px");
+
+            if (major == this.config.min || major == this.config.max) {
+                var point = this.valueToPoint(major, 0.63);
+                var refTextConfig = {
+                    size: Math.round(this.config.size / 16),
+                    x: point.x,
+                    y: point.y,
+                    anchor: major == this.config.min ? "start" : "end"
+                };
+
+                this.appendText(this.svg, major, refTextConfig);
+            }
+        }
+
+        var pointerContainer = this.svg.append("g").attr("class", "pointerContainer");
+
+        this.drawPointer(this.value);
+        pointerContainer.append("circle")
+        .attr("cx", this.config.size / 2)
+        .attr("cy", this.config.size / 2)
+        .attr("r", 0.12 * this.config.radius)
+        .style("fill", "#4684EE")
+        .style("stroke", "#666")
+        .style("opacity", 1);
+    };
+
+    this.put = function(value) {
+        this.value = value;
+    };
+
+    this.draw = function() {
+        this.drawPointer(this.value);
+    };
+
+    this.drawBand = function(start, end, color) {
+        if (0 >= end - start) return;
+
+        this.svg.append("path")
+        .style("fill", color)
+        .attr("d", d3.svg.arc()
+            .startAngle(this.valueToRadians(start))
+            .endAngle(this.valueToRadians(end))
+            .innerRadius(0.65 * this.config.radius)
+            .outerRadius(0.85 * this.config.radius))
+        .attr("transform", function() { return "translate(" + self.config.size / 2 + ", " + self.config.size / 2 + ") rotate(270)"; });
+    };
+
+    this.drawPointer = function(value) {
+        var delta = this.config.range / 13,
+            head = this.valueToPoint(value, 0.85),
+            head1 = this.valueToPoint(value - delta, 0.12),
+            head2 = this.valueToPoint(value + delta, 0.12),
+
+            tailValue = value -  (this.config.range * (1/(270/360)) / 2),
+            tail = this.valueToPoint(tailValue, 0.28),
+            tail1 = this.valueToPoint(tailValue - delta, 0.12),
+            tail2 = this.valueToPoint(tailValue + delta, 0.12),
+
+            data = [head, head1, tail2, tail, tail1, head2, head],
+
+            line = d3.svg.line()
+                .x(function(d) { return d.x; })
+                .y(function(d) { return d.y; })
+                .interpolate("basis"),
+
+            pointerContainer = this.svg.select(".pointerContainer"),
+            pointer = pointerContainer.selectAll("path").data([data]);
+
+        pointer.enter()
+            .append("path")
+            .attr("d", line)
+            .style("fill", "#dc3912")
+            .style("stroke", "#c63310")
+            .style("fill-opacity", 0.7);
+
+        pointer.transition()
+            .attr("d", line)
+            .ease("linear")
+            .duration(100);
+
+        var container =    pointerContainer.selectAll("text")
+            .data([value])
+            .text(Math.round(value))
+            .enter();
+
+        this.appendText(container, Math.round(value), this.config.valueTextConfig);
+    };
+
+    this.valueToDegrees = function(value) {
+        return value / this.config.range * 270 - 45;
+    };
+
+    this.valueToRadians = function(value) {
+        return this.valueToDegrees(value) * Math.PI / 180;
+    };
+
+    this.valueToPoint = function(value, factor) {
+        var point = {
+            x: this.config.size / 2 - this.config.radius * factor * Math.cos(this.valueToRadians(value)),
+            y: this.config.size / 2 - this.config.radius * factor * Math.sin(this.valueToRadians(value))
+        };
+
+        return point;
+    };
+
+    this.appendText = function(obj, text, textConfig) {
+        obj.append("text")
+            .attr("x", textConfig.x)
+            .attr("y", textConfig.y)
+            .attr("dy", textConfig.size / 2)
+            .attr("text-anchor", textConfig.anchor)
+            .text(text)
+            .style("font-size", textConfig.size + "px")
+            .style("fill", "#333")
+            .style("stroke-width", "0px");
+    };
+}
---
+++ frontend/widgets/graph.js
@@ -0,0 +1,92 @@
+function Graph(width, height, maxDataSize) {
+
+    var dataCount = 0, // keep track of the number of data points we receive
+                       // when we reach maxDataSize, we stop scaling x-axis and start scrolling
+        data = zeros(maxDataSize),
+
+        labelMargin = {top: 10, right: 10, bottom: 20, left: 60};
+        // default to 0 x/y domain, auto-scale as new data comes in
+        xDomain = [0, 0],
+        yDomain = [Number.MAX_VALUE, Number.MIN_VALUE],
+
+        mapX = d3.scale.linear().domain(xDomain).range([0, width]),
+        mapY = d3.scale.linear().domain([0, 0]).range([height, 0]),
+
+        xAxis = d3.svg.axis().scale(mapX).orient("bottom"),
+        yAxis = d3.svg.axis().scale(mapY).orient("left"),
+
+        line = d3.svg.line()
+        .x(function(d, i) { return mapX(xDomain[1] - i); })
+        .y(function(d) { return mapY(d); }),
+
+        area = d3.svg.area()
+        .x(function(d, i) { return mapX(xDomain[1] - i); })
+        .y0(height)
+        .y1(function(d) { return mapY(d); }),
+
+        svg = d3.select("body").append("svg")
+            .attr("width", width + labelMargin.left + labelMargin.right)
+            .attr("height", height + labelMargin.top + labelMargin.bottom)
+            .style("background-color", "white")
+            .append("g")
+            .attr("transform", "translate(" + labelMargin.left + "," + labelMargin.top + ")"),
+        paths = [new Path(svg, data, "line", line),
+                 new Path(svg, data, "area", area)];
+
+    svg.append("defs").append("clipPath")
+        .attr("id", "clip")
+        .append("rect")
+        .attr("width", width)
+        .attr("height", height);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+    .attr("class", "y axis")
+        .call(yAxis);
+
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", -labelMargin.left)
+        .attr("x", -height / 2)
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Magnitude");
+
+    // add data to the list
+    this.put = function(value) {
+        // pop the old data point off the front
+        data.pop();
+
+        data.unshift(value);
+
+        // "scale" (widen) domain until we reach the specified max-datapoints to have on the graph,
+        // then start "translating" (keeping domain width, but tracking present data points)
+        xDomain[1] = ++dataCount;
+        if (dataCount >= maxDataSize) {
+            xDomain[0] = xDomain[1] - maxDataSize;
+        }
+        // y-domain should contain all data points.
+        yDomain[0] = Math.min(value, yDomain[0]);
+        yDomain[1] = Math.max(value, yDomain[1]);
+
+        // remap to new x/y domains
+        mapX.domain(xDomain);
+        mapY.domain(yDomain);
+
+        // update x/y-axis labels/ticks
+        svg.select(".x.axis").call(xAxis);
+        svg.select(".y.axis").transition().duration(100).call(yAxis);
+
+    };
+
+    // redraw each path (line & area)
+    this.draw = function() {
+        paths.forEach(function(path) {
+            path.draw(0);
+        });
+    };
+}
